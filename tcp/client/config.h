856a6fe550a681994700e1e23c46d6768aa5f64e
izaias junior <izaiasjunior@MacBook-Air-de-izaias.local>
2021-03-21 22:19:40 -0300

    change ip serverr

--- tcp/client/config.h
+++ tcp/client/config.h
@@ -6,14 +6,8 @@
 #define STAPSK  "7AVqt9RcXX"
 #endif
 
-// Set your Static IP address
-IPAddress server_IP(192, 168, 5, 10);
-IPAddress gateway(192, 168, 5, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4);
 
-const char* host = "192.168.5.10";
+const char* host = "192.168.0.10";
 const uint16_t port = 80;
 
 const char* ssid     = STASSID;
