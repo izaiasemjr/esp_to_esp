1316b48e5d1ea81e958a2e95dc60ca49389a6bf2
izaiasemjr <izaias.machado@unimefortaleza.com.br>
2021-03-24 00:47:24 -0300

    adjustments in server

--- tcp/server/server.ino
+++ tcp/server/server.ino
@@ -1,5 +1,6 @@
 
 #include "config.h"
+#define PIN_SAIDA 2
 
 
 // Create an instance of the server
@@ -9,9 +10,8 @@ WiFiServer server(port);
 void setup() {
   Serial.begin(115200);
 
-  // prepare LED
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, 0);
+  pinMode(PIN_SAIDA, OUTPUT);
+  digitalWrite(PIN_SAIDA, 0);
 
   // Connect to WiFi network
   Serial.println();
@@ -40,8 +40,13 @@ void setup() {
 
   // Print the IP address
   Serial.println(WiFi.localIP());
+
+  // prepare LED
+  pinMode(PIN_SAIDA, OUTPUT);
+  digitalWrite(PIN_SAIDA, 0);
 }
 
+String code;
 void loop()
 {
   WiFiClient client = server.available();
@@ -53,70 +58,21 @@ void loop()
     }
 
     while (client.connected()) {
+      code="";
       while (client.available() > 0) {
         // read data from the connected client
-        Serial.write(client.read());
+        char ch = client.read();
+        code+=ch; 
       }
-      //Send Data to connected client
-      while (Serial.available() > 0)
-      {
-        client.write(Serial.read());
+
+      if(code!=""){
+        Serial.print("code:");
+        Serial.println(code);
+        digitalWrite(PIN_SAIDA, int(code[2]));
       }
+   
     }
     client.stop();
     Serial.println("Client disconnected");
   }
 }
-
-//void loop() {
-//  // Check if a client has connected
-//  WiFiClient client = server.available();
-//  if (!client) {
-//    return;
-//  }
-//  Serial.println(F("new client"));
-//
-//  client.setTimeout(5000); // default is 1000
-//
-//  // Read the first line of the request
-//  String req = client.readStringUntil('\r');
-//  Serial.println(F("request: "));
-//  Serial.println(req);
-//
-//  // Match the request
-//  int val;
-//  if (req.indexOf(F("/gpio/0")) != -1) {
-//    val = 0;
-//  } else if (req.indexOf(F("/gpio/1")) != -1) {
-//    val = 1;
-//  } else {
-//    Serial.println(F("invalid request"));
-//    val = digitalRead(LED_BUILTIN);
-//  }
-//
-//  // Set LED according to the request
-//  digitalWrite(LED_BUILTIN, val);
-//
-//  // read/ignore the rest of the request
-//  // do not client.flush(): it is for output only, see below
-//  while (client.available()) {
-//    // byte by byte is not very efficient
-//    client.read();
-//  }
-//
-//  // Send the response to the client
-//  // it is OK for multiple small client.print/write,
-//  // because nagle algorithm will group them into one single packet
-//  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now "));
-//  client.print((val) ? F("high") : F("low"));
-//  client.print(F("<br><br>Click <a href='http://"));
-//  client.print(WiFi.localIP());
-//  client.print(F("/gpio/1'>here</a> to switch LED GPIO on, or <a href='http://"));
-//  client.print(WiFi.localIP());
-//  client.print(F("/gpio/0'>here</a> to switch LED GPIO off.</html>"));
-//
-//  // The client will actually be *flushed* then disconnected
-//  // when the function returns and 'client' object is destroyed (out-of-scope)
-//  // flush = ensure written data are received by the other side
-//  Serial.println(F("Disconnecting from client"));
-//}
